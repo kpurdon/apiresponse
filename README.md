f493695c218c2914fc106f120b66eae26a9ed288
Kyle Purdon <kylepurdon@gmail.com>
2018-12-11 11:30:30 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+apiresponse
+-----
+
+WIP
---
+++ apiresponse.go
@@ -0,0 +1,47 @@
+package apiresponse
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Responder struct {
+	w http.ResponseWriter
+}
+
+func NewResponder(w http.ResponseWriter) *Responder {
+	return &Responder{w: w}
+}
+
+func (r Responder) write(statusCode int, data interface{}) {
+	if data == nil {
+		// TODO: something more useful?
+		data = struct {
+			StatusCode int    `json:"status_code"`
+			StatusText string `json:"status_text"`
+		}{
+			StatusCode: statusCode,
+			StatusText: http.StatusText(statusCode),
+		}
+	}
+
+	b, err := json.Marshal(data)
+	if err != nil {
+		r.InternalServerError()
+		return
+	}
+
+	r.w.Header().Set("Content-Type", "application/json")
+	r.w.WriteHeader(statusCode)
+	r.w.Write(b)
+}
+
+// TODO: generate these based on http.Status*
+
+func (r Responder) OK(data interface{}) {
+	r.write(http.StatusOK, data)
+}
+
+func (r Responder) InternalServerError() {
+	r.write(http.StatusInternalServerError, nil)
+}
